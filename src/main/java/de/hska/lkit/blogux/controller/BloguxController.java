79da5a97c8d8d783028305d3109a9f9dfd20e6e9
AzizShaumarov <aziz.shaumarov@gmail.com>
2018-04-10 22:45:36 +0200

    added main template bootstrap updated to 4.0.0

--- src/main/java/de/hska/lkit/blogux/controller/BloguxController.java
+++ src/main/java/de/hska/lkit/blogux/controller/BloguxController.java
@@ -5,15 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import de.hska.lkit.blogux.model.Signup;
+import de.hska.lkit.blogux.model.*;
+
 
 @Controller
-public class SignupController {
+public class BloguxController {
 
 	@RequestMapping(value = "/signup")
 	public String signupSubmit(@ModelAttribute Signup signup, Model model) {
 		model.addAttribute("signup", signup != null ? signup : new Signup());
 		return "signup";
+	}
+
+	@RequestMapping(value = "/")
+	public String signupSubmit(@ModelAttribute Home home, Model model) {
+		model.addAttribute("home", home != null ? home : new Home());
+		return "main_template";
 	}
 
 }
