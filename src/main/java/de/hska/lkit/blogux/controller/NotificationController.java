c55698dce3601b97ba5ffd214b48ea8ed65db6c4
Aziz Shaumarov <aziz.shaumarov@gmail.com>
2018-06-19 19:33:21 +0200

    Added some comments to last commit

--- src/main/java/de/hska/lkit/blogux/controller/NotificationController.java
+++ src/main/java/de/hska/lkit/blogux/controller/NotificationController.java
@@ -3,9 +3,7 @@ package de.hska.lkit.blogux.controller;
 import org.springframework.messaging.handler.annotation.Payload;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import de.hska.lkit.blogux.socket.pojos.NewPostMsg;
-import de.hska.lkit.blogux.socket.pojos.NewPostNotification;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -14,18 +12,18 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 public class NotificationController {
 
   @Autowired
-  private SimpMessagingTemplate messagingStopmTemplate;
-
-  @Autowired
   private StringRedisTemplate stringRedisTemplate;
 
    @MessageMapping("/newpost")
    public void notify(@Valid @Payload NewPostMsg message) throws Exception {
 
      // send directly throug STOMP at same Application
+     // -> here occurs Problem with double Messaging if Client go throug same App-Instanse
      // messagingStopmTemplate.convertAndSend("/topic/" + message.getName(), new NewPostNotification(message.getName(), message.getMsg()));
-     // here occurs Problem with double Messaging if Client go throug same App-Instanse
+
+
      // send directly in RedisQueue
+     // it goes first in redisQueue only after in STOPMquee. Inefficient
      String redisQueue = patterToSendRedisQueue(message.getName(), message.getMsg());
      stringRedisTemplate.convertAndSend("redisChannel", redisQueue);
    }
